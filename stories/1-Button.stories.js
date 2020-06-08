2034fa12a69259fb612d11e238c1eea1a4ddb9aa
kahasan <kahraman.h@outlook.com>
2020-06-08 20:48:20 +0300

    Create buttons for navigation

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
 import Button from '../components/button';
+import NavigationButton from '../components/navigation-button';
+import Navigation from '../components/navigation';
 
 export default {
   title: 'Buttons',
 };
 
 export const Normal = () => <Button>Click me!</Button>;
+
+export const NavButton = () => <NavigationButton>Click me!</NavigationButton>;
+
+export const Nav = () => <Navigation />;
