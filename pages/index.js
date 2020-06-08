dce988dfc1fc93a3e1a4fc02aa622c093648b0b0
kahasan <kahraman.h@outlook.com>
2020-06-08 18:40:00 +0300

    Initial commit
    Setup next and storybook

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/button';
+
+function HomePage() {
+  return (
+    <div>
+      <Button>Merhaba</Button>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ stories/1-Button.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '../components/button';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Normalbtn = () => <Button>Click me!</Button>;
